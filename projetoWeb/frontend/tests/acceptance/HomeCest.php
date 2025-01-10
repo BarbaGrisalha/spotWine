454ea9dcd2627127071b36e38cb1e89de0157c40
Altamir Rodrigues <box.altamir@gmail.com>
2025-01-10 09:48:58 +0000

    Ajustes do frontend

--- projetoWeb/frontend/tests/acceptance/HomeCest.php
+++ projetoWeb/frontend/tests/acceptance/HomeCest.php
@@ -12,9 +12,19 @@ class HomeCest
         $I->amOnPage('/site/index');
         $I->see('Login');
         $I->seeLink('Vinhos');
+
         $I->click('Vinhos');
-        $I->wait(2); // wait for page to be opened
-        $I->see('O Mercado de Vinhos Diretamente das Caves');
+        $I->wait(2); // Aguarda o carregamento da página
+
+        // Garantir que o elemento existe antes de clicar
+        $I->seeElement('img', ['alt' => 'Imagem garrafa de vinho']);
+
+        // Clicar diretamente no elemento via XPath
+        $I->click('//img[@alt="Imagem garrafa de vinho"]');
+        $I->wait(4);
+
+        $I->click('Adicionar ao Carrinho');
+        $I->wait(4);
 
     }
 }
